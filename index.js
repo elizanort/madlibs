ebd4a176a14e5bf2d0f65c28b287a786b93ee1d9
“elizanort” <elizabethnortman@gmail.com>
2020-03-30 15:12:45 -0400

    created madlib story

---
+++ index.js
@@ -0,0 +1,20 @@
+    let rockType = prompt ("Name a type of rock");
+    let numberAnimal = prompt ("Pick a number");
+    let animal = prompt("Name a kind of animal");
+    let color = prompt("Choose a color");
+    let houseLocation = prompt("Name a place in your house");
+    let hotDrink = prompt ("Name a hot drink")
+    let food = prompt("Name a food");
+    let number2 = prompt ("Pick another number");
+    let verb = prompt ("Pick a verb, ending in -ed");
+    let vacationLocation = prompt ("Name a place people go on vacation");
+    let hurtNoise = prompt ("A noise somone might make when their hurt");
+    let timeLimit = prompt ("Pick a period of time under 24 hours")
+
+    document.write("There once was a story of a man who lived under a " + rockType + " rock! <br/>");  
+    document.write("This man did not live under the " + rockType + " rock alone, he lived quite comfortable with " + numberAnimal + " " + animal + "s . <br/>");  
+    document.write("This man went by the name " + color + ". And " + color + "spent his mornings in the" + houseLocation + " drinking his favorite drink: " + hotDrink + ", and eating his favorite food: " + food + " . <br/>");
+    document.write("After he completed " + number2 + " servings he " + verb + " himself outside to tan. It was always nice at his home because he lived in" + vacationLocation + " . </br>");
+    document.write("However, he had to be careful. Too much sun made him say" + hurtNoise + "!!!! Really loudly for all to hear.");
+    document.write("He now limits his exposure to only " + timeLimit + "so that he doesn't get crispy! </br>");
+    document.write("And... That's the story of" + color + " the man who lived under a " + rockType + " rock!");
